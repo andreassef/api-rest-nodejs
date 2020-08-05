3269a1c493e70fc0539e588c1673aa6a4c01a283
andreassef <andreassef02@gmail.com>
2020-08-05 17:54:16 -0400

    Aula2- implementando rotas- NextLevelWeek - RocketSeat

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express, { response } from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 //Corpo (Request Body): Dados para criação ou atualização
 // Route Params: Identificar qual recursos eu quero atualizar ou deletar
 // Query Params: Paginação, filtros, ordenação
-
-app.post('/users', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
 
 app.listen(3333);
